b5bc2a4b6eb490f689140a5a50052ef8f4cf560d
bberzhou <bberzhou@gmail>
2018-12-07 22:08:57 +0800

    added two component

--- src/App.js
+++ src/App.js
@@ -1,28 +1,56 @@
-import React, { Component } from 'react';
+import React, {Component} from 'react';
+//  使用Fragment可以将最外层的隐藏掉
 import logo from './logo.svg';
+import MyComponent from './MyComponent';
+import Table from './Table';
 import './App.css';
 
+// import {Component} from 'react';
+//  等价于
+// import React from 'react';
+// const Component = React.Component
+
+//  React DOM 也只会更新渲染文本节点中发生变化的内容。
+
+
+//  类app继承了component就成为了一个组件
 class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
-    );
-  }
+
+    // render也是JSX的语法，在写JSX的时候一般的都会带上一个小括号，防止分号自动插入
+    render() {
+        // render函数返回的就是这个组件展示的内容
+        return (
+
+            <div>
+            <div className="App">
+                <header className="App-header">
+                    {/*
+                        在JSX里面可以以引号来定义以字符串为值的属性，也可以使用使用大块号来定义以JS表达式为值的属性
+                    */}
+                    <img src={logo} className="App-logo" alt="logo"/>
+                    <p>
+                        Edit <code>src/App.js</code> and save to reload.
+                    </p>
+                    <a
+                        className="App-link"
+                        href="https://reactjs.org"
+                        target="_blank"
+                        rel="noopener noreferrer"
+                    >
+                        Learn React
+                    </a>
+                    <p>hello world</p>
+                </header>
+                <div>
+                    <MyComponent name="DDD"/>
+                    <Table/>
+                </div>
+                </div>
+            </div>
+
+        );
+    }
+
 }
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -1,10 +1,22 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+//  all in js，CSS也可以像js一样直接引入，
 import './index.css';
+//  引入app.js会直接补充后缀
 import App from './App';
+
+
+//  progressive web application,引入serviceWorker就是为了写手机app
+
+//  支持https的设备如果断网了的话，第二次再访问的时候也能够看到之前的内容,会缓存在浏览器里面
 import * as serviceWorker from './serviceWorker';
 
+
+//  JSX 语法一定需要引入React
+//  在JSX中如果需要使用自己的组件时，标签要以大写字母开头
 ReactDOM.render(<App />, document.getElementById('root'));
+
+
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
